822f1cbfffbd31920c3c8cd44d31137fbdfd9e0c
Hanbrolo117 <hanbrolo117@gmail.com>
2017-07-22 19:33:26 -0700

    Stubbed out VoteCommand subTypes, implemented VoteCommand factory.

--- discordapp/src/main/java/com/markandkyle/discordbot/models/Command.java
+++ discordapp/src/main/java/com/markandkyle/discordbot/models/Command.java
@@ -3,14 +3,6 @@ package com.markandkyle.discordbot.models;
 /**
  * Created by CptAmerica on 7/22/17.
  */
-public class Command {
-    private String name;
-
-    public Command(String name) {
-
-    }
-
-    //Uses parser to grab command data for execution.
-    public void execute() {}
-    public void parser() {}
+public interface Command {
+    public void execute();
 }
