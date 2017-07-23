cb0ff9668b7e108593e3879da50e348e933d3913
mhilliker <mhilliker@localhost>
2017-07-23 01:09:38 -0500

    resolved merge conflicts...?

---
+++ discordapp/src/main/java/com/markandkyle/discordbot/models/commands/StartVoteCommand.java
@@ -0,0 +1,23 @@
+package com.markandkyle.discordbot.models.commands;
+
+import com.markandkyle.discordbot.MessageHandler;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+
+/**
+ * Created by CptAmerica on 7/22/17.
+ */
+public class StartVoteCommand extends VoteCommand {
+
+    StartVoteCommand(MessageHandler handler, MessageReceivedEvent event) {
+        super(handler, event);
+    }
+
+    @Override
+    public void execute(String message) {
+        msgHandler.sendMessage("Start Vote Command Created! Content: " + removeStartCommand(message), event);
+    }
+
+    public String removeStartCommand(String msg){
+        return removeVoteCommand(msg).replaceFirst("start","");
+    }
+}
